54ba87f2529fdb640a577fd57e31bfee16c9b121
Ulrike Gartler <ulrike.gartler@AXTESYS.local>
2021-07-24 19:36:03 +0200

    all files

---
+++ src/screens/MainCategoryScreen.js
@@ -0,0 +1,56 @@
+import React, {  } from "react";
+import { connect } from "react-redux";
+import { Button } from "../core-components/Button";
+import { Link } from "react-router-dom";
+import firebase from "firebase";
+import {
+  takeMotivation,
+  takeStrategy,
+} from "../redux/actions/mainCategoryActions";
+
+export function MainCategoryScreen(props) {
+  console.log(props);
+
+ 
+
+  
+
+  return (
+    <div className="screen">
+      {/* <h2>Decision- {props.mainCategoryDecision}</h2> */}
+      <label>
+        Sehr gerne :) <br></br>
+        Welche Art von Unterstützung brauchst du?
+      </label>
+      <br></br>
+     
+     
+      <br></br>
+      <Link to="/subCategory">
+        <Button text="Strategien" function={props.takeStrategy}></Button>
+      </Link>
+      
+     
+      <br></br>
+      <Link to="/subCategory">
+        <Button text="Motivation" function={props.takeMotivation}></Button>
+      </Link>
+    </div>
+  );
+}
+
+//global state from store
+const mapStateToProps = (state) => {
+  return {
+    mainCategoryDecision: state.main.mainCategoryDecision,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    takeStrategy: () => dispatch(takeStrategy()),
+    takeMotivation: () => dispatch(takeMotivation()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainCategoryScreen);
